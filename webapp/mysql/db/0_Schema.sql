80a4b9bf54f9ae573b3f6756ed6aec6c5dcb8446
kei_yonekawa <kei_yonekawa@m4.local>
2025-11-12 22:26:28 +0900

    fix

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -7,23 +7,30 @@ DROP TABLE IF EXISTS isuumo.chair;
 CREATE TABLE isuumo.estate
 (
     id          INTEGER             NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)         NOT NULL,
+    name        TEXT         NOT NULL,
     description VARCHAR(4096)       NOT NULL,
     thumbnail   VARCHAR(128)        NOT NULL,
-    address     VARCHAR(128)        NOT NULL,
+    address     TEXT                NOT NULL,
     latitude    DOUBLE PRECISION    NOT NULL,
     longitude   DOUBLE PRECISION    NOT NULL,
     rent        INTEGER             NOT NULL,
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
-    features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    features    TEXT                NOT NULL,
+    popularity  INTEGER             NOT NULL,
+
+    INDEX idx_rent_id (rent, id),
+    INDEX idx_popularity_id (popularity, id)
+    -- INDEX idx_latitude (latitude),
+    -- INDEX idx_longitude (longitude),
+    -- INDEX idx_door_width (door_width),
+    -- INDEX idx_door_height (door_height)
 );
 
 CREATE TABLE isuumo.chair
 (
     id          INTEGER         NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)     NOT NULL,
+    name        TEXT    NOT NULL,
     description VARCHAR(4096)   NOT NULL,
     thumbnail   VARCHAR(128)    NOT NULL,
     price       INTEGER         NOT NULL,
@@ -31,8 +38,16 @@ CREATE TABLE isuumo.chair
     width       INTEGER         NOT NULL,
     depth       INTEGER         NOT NULL,
     color       VARCHAR(64)     NOT NULL,
-    features    VARCHAR(64)     NOT NULL,
+    features    TEXT            NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+
+    INDEX idx_stock_price_id (stock, price, id),
+    INDEX idx_stock_popularity_id (stock, popularity, id)
+    -- INDEX idx_kind (kind),
+    -- INDEX idx_color (color),
+    -- INDEX idx_height (height),
+    -- INDEX idx_width (width),
+    -- INDEX idx_depth (depth)
 );
--- webapp/nodejs/app.js
+++ webapp/nodejs/app.js
@@ -26,6 +26,7 @@ const dbinfo = {
   password: process.env.MYSQL_PASS ?? "isucon",
   database: process.env.MYSQL_DBNAME ?? "isuumo",
   connectionLimit: 10,
+  charset: "utf8mb4",
 };
 
 const app = express();
@@ -45,7 +46,7 @@ app.post("/initialize", async (req, res, next) => {
     const execfiles = dbfiles.map((file) => path.join(dbdir, file));
     for (const execfile of execfiles) {
       await exec(
-        `mysql -h ${dbinfo.host} -u ${dbinfo.user} -p${dbinfo.password} -P ${dbinfo.port} ${dbinfo.database} < ${execfile}`
+        `mysql --default-character-set=utf8mb4 -h ${dbinfo.host} -u ${dbinfo.user} -p${dbinfo.password} -P ${dbinfo.port} ${dbinfo.database} < ${execfile}`
       );
     }
     res.json({
